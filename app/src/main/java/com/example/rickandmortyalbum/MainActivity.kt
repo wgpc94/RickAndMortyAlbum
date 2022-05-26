b4f42871db1f7d862aac12dcb7f089bf5e9a0c7d
Welington Gomes <Welingtonsc94@gmail.com>
2022-05-26 10:35:04 -0300

    created layouts, fragments and menu

--- app/src/main/java/com/example/rickandmortyalbum/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyalbum/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.rickandmortyalbum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.rickandmortyalbum.view.CharactersListFragment
+import com.example.rickandmortyalbum.view.FragmentAbout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fill, CharactersListFragment())
+            commit()
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_more, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.m_first -> supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fill, FragmentAbout())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        return true
+    }
+
+
 }
---
+++ app/src/main/java/com/example/rickandmortyalbum/view/CharacterFragment.kt
@@ -0,0 +1,20 @@
+package com.example.rickandmortyalbum.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.rickandmortyalbum.R
+
+class CharacterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return  inflater.inflate(R.layout.fragment_character_detail, container, false)
+    }
+
+}
